3529608e9423287d2470d60c2d087002d22cb644
Abdul Waheed <waheedjdk@hotmail.com>
2024-04-18 16:51:55 +0500

    Guess Birthday App Command-Line Application.

---
+++ README.md
@@ -0,0 +1,46 @@
+# Guess Birthday App
+
+## Description
+The Guess Birthday App is a command-line application that helps users determine their birthday based on a series of questions. It presents users with five sets of numbers, each representing a group of days in a month. Users simply answer whether their birthday falls in each set by entering 0 for No or 1 for Yes. Based on their answers, the app calculates and displays their birthday.
+
+## Features
+- Simple and intuitive interface
+- Provides an interactive way to guess birthdays
+- Utilizes ASCII art for a visually appealing experience
+- Written in TypeScript for robustness and maintainability
+
+## Technologies Used
+- Node.js
+- TypeScript
+- Inquirer.js for interactive command-line prompts
+- Chalk for styling the command-line output
+- Figlet for creating ASCII art
+
+## Installation
+1. Clone the repository:
+   ```sh
+   git clone https://github.com/Al-Balushi/guess-birthday-app.git
+   ```
+2. Navigate to the project directory:
+   ```sh
+   cd guess-birthday-app
+   ```
+3. Install dependencies:
+   ```sh
+   npm install
+   ```
+
+## Usage
+1. Run the app:
+   ```sh
+   npm start
+   ```
+2. Follow the on-screen instructions to guess your birthday.
+
+## License
+This project is licensed under the ISC License.
+
+## Acknowledgements
+- This app was created as part of a learning project by Abdul Waheed.
+- Special thanks to the developers of Inquirer.js, Chalk, and Figlet for their fantastic libraries.
+
---
+++ index.ts
@@ -0,0 +1,72 @@
+#! /usr/bin/env node
+import * as readlineSync from 'readline-sync';
+import inquirer from "inquirer";
+import chalk from "chalk";
+import figlet from "figlet";
+import gradient from "gradient-string";
+
+let todoList: string[] = [];
+let conditions = true;
+
+// Print welcome message with ASCII art and colorful gradient
+console.log(
+  gradient.instagram(figlet.textSync("Code with Abdul Waheed", { horizontalLayout: "full" }))
+);
+const set1 =
+  " 1 3 5 7\n" +
+  " 9 11 13 15\n" +
+  "17 19 21 23\n" +
+  "25 27 29 31";
+
+const set2 =
+  " 2 3 6 7\n" +
+  "10 11 14 15\n" +
+  "18 19 22 23\n" +
+  "26 27 30 31";
+
+const set3 =
+  " 4 5 6 7\n" +
+  "12 13 14 15\n" +
+  "20 21 22 23\n" +
+  "28 29 30 31";
+
+const set4 =
+  " 8 9 10 11\n" +
+  "12 13 14 15\n" +
+  "24 25 26 27\n" +
+  "28 29 30 31";
+
+const set5 =
+  "16 17 18 19\n" +
+  "20 21 22 23\n" +
+  "24 25 26 27\n" +
+  "28 29 30 31";
+
+let day = 0;
+
+console.log(chalk.cyan("\nIs your birthday in Set1?"));
+console.log(chalk.green(set1));
+let answer = readlineSync.questionInt("Enter 0 for No and 1 for Yes: ");
+if (answer === 1) day += 1;
+
+console.log(chalk.cyan("\nIs your birthday in Set2?"));
+console.log(chalk.green(set2));
+answer = readlineSync.questionInt("Enter 0 for No and 1 for Yes: ");
+if (answer === 1) day += 2;
+
+console.log(chalk.cyan("\nIs your birthday in Set3?"));
+console.log(chalk.green(set3));
+answer = readlineSync.questionInt("Enter 0 for No and 1 for Yes: ");
+if (answer === 1) day += 4;
+
+console.log(chalk.cyan("\nIs your birthday in Set4?"));
+console.log(chalk.green(set4));
+answer = readlineSync.questionInt("Enter 0 for No and 1 for Yes: ");
+if (answer === 1) day += 8;
+
+console.log(chalk.cyan("\nIs your birthday in Set5?"));
+console.log(chalk.green(set5));
+answer = readlineSync.questionInt("Enter 0 for No and 1 for Yes: ");
+if (answer === 1) day += 16;
+
+console.log(chalk.yellowBright(`\nYour birthday is ${day}!`));
